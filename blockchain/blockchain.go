50e58790f62b764aea2ca25a87830b6149cb18ca
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2018-11-22 22:44:13 +0100

    initial look for end of line

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,8 +7,13 @@ import (
 
 
 func main() {
-	genesisTransactions := []string{"Hello","hi"}
-	genesisBlock := block.NewBlock( genesisTransactions, nil )
+	genesisTransactions := []string{"Hello", "hi"}
+	genesisBlock := block.NewBlock(genesisTransactions, nil)
 	fmt.Printf("%+v \n", genesisBlock.Hash)
 	fmt.Println("FINISH")
+
+	newTransactions := []string{"Aloha", "Hola"}
+	secondBlock := block.NewBlock(newTransactions, genesisBlock)
+	fmt.Println(secondBlock.Hash)
 }
+
