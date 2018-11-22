e8c6d64fa6248eb6615265129cd7c96c2353a844
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2018-11-22 22:43:38 +0100

    block contains method to hash and basic mining features

--- block/block.go
+++ block/block.go
@@ -2,27 +2,59 @@ package block
 
 import (
 	"crypto/sha512"
-	"strings"
 )
 
+// Transaction contains the information about a transaction int eh blockchain
+// TODO inmplement actual transaction
+type Transaction string
+
+// Transaction contains information about a transaction in the blockchain
+type mempool []Transaction
+
+// Block is the basic unit in the blockchain, it contains a number of transactions
+// and its corresponding hash, previous block
 type Block struct {
-	Transactions  []string
+	Transactions  []Transaction // a transaction is a string in v.alpha
 	Hash          string
 	PreviousBlock *Block
 }
 
-func NewBlock(transactions []string, previousBlock *Block) *Block {
+// NewBlock creates a new block with the set of given transactions and the
+// given previous block
+func NewBlock(transactionPool *mempool, previousBlock *Block) *Block {
 
 	currentBlock := Block{}
 	currentBlock.PreviousBlock = previousBlock
-	currentBlock.Transactions = transactions
 
-	transactionsContent := strings.Join(currentBlock.Transactions, "")
+	// First, select which transactions the block will contain
+	selectedTransactions := selectTransactions(transactionPool)
+	currentBlock.Transactions = selectedTransactions
 
-	harsher := sha512.New()
-	harsher.Write([]byte(transactionsContent))
-	currentBlock.Hash = string(harsher.Sum([]byte{}))
-
+	// Second, calculate the hash of the selected transactions
+	hashedTransaction := string(processTransactions(selectedTransactions))
+	hashedBlockData := hashedTransaction + currentBlock.Hash
+	currentBlock.Hash = hashedBlockData
 	return &currentBlock
 }
 
+func selectTransactions(transactionPool *mempool) []Transaction {
+	return []Transaction{}
+}
+
+// resolveTransaction hashes a specific transaction
+// and returns the new value in the form of bytes
+func resolveTransaction(transaction Transaction) []byte {
+	transactionData := []byte(transaction)
+	return transactionData
+}
+
+// processTransactions resolves a new block from the given mempool
+// it is in charge of selecting which trasactions to process
+func processTransactions(selectedTransactions []Transaction) []byte {
+	harsher := sha512.New()
+	for _, v := range selectedTransactions {
+		transformedTransaction := resolveTransaction(v)
+		harsher.Write(transformedTransaction)
+	}
+	return harsher.Sum([]byte{})
+}
